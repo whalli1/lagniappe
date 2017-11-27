f8a7ec3f0d983d91241faf46d9984bc008501860
David Burns <davidburns612@gmail.com>
2017-11-27 13:33:49 -0600

    Update feed.js
    basic flatlist. Working on implementing event class so I can show some real events in feed.

--- screens/feed.js
+++ screens/feed.js
@@ -1,18 +1,50 @@
 import { AppRegistry } from 'react-native';
 import React, { Component } from 'react';
 import { Friends } from './friends'
-import { Container, Header, Content, Footer, FooterTab, Button, Text, Icon, Title } from 'native-base';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Title } from 'native-base';
+import {FlatList, ActivityIndicator, View, Text, StyleSheet} from 'react-native';
+
 
 export default class FeedPage extends Component {
 
-    render() {
-        return (
-            <Container>
-                <Text>
-                    This is Feed
-                </Text>
-            </Container>
-        );
-    }
-
+	  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+            {key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+			{key: 'Events'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    );
+  }
 }
+//need to implement renderitem(event) 
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 72,
+    fontSize: 32,
+    height: 72,
+  },
+})
